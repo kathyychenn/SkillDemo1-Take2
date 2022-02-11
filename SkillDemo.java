cb02793858813699a82799f17051ec7f3463542d
kathyychenn <97640717+kathyychenn@users.noreply.github.com>
2022-02-11 13:15:56 -0800

    skilldemo 1 video

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,5 @@
-public class SkillDemo{} // write later in vs code
+public class SkillDemo{
+    public static String birthday(int age){
+        return "happy " + age + "th birthday";
+    }
+} // write later in vs code
